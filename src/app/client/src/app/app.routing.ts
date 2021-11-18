aec7de0b01b5b64a0465fc2fee4746e6187d444e
Kartheek Palla <palla.kartheekreddy@gmail.com>
2021-11-18 22:52:12 +0530

    Issue #SB-000 fix: removed unused modules

--- src/app/client/src/app/app.routing.ts
+++ src/app/client/src/app/app.routing.ts
@@ -3,25 +3,13 @@ import { ErrorPageComponent, AuthGuard } from '@sunbird/core';
 import { RouterModule, Routes } from '@angular/router';
 const appRoutes: Routes = [
   {
-    path: 'resources', loadChildren: () => import('app/modules/resource/resource.module').then(m => m.ResourceModule)
-  },
-  {
-    path: 'search', loadChildren: () => import('app/modules/search/search.module').then(m => m.SearchModule)
-  },
-  {
     path: 'sourcing', loadChildren: () => import('app/modules/program/program.module').then(m => m.ProgramModule)
   },
   {
     path: 'contribute', loadChildren: () => import('app/modules/contribute/contribute.module').then(m => m.ContributeModule)
   },
   {
-    path: 'dashBoard', loadChildren: () => import('app/modules/dashboard/dashboard.module').then(m => m.DashboardModule)
-  },
-  {
     path: 'profile', loadChildren: () => import('app/plugins/profile/profile.module').then(m => m.ProfileModule)
-  },
-  {
-    path: 'certs', loadChildren: () => import('app/modules/certificate/certificate.module').then(m => m.CertificateModule)
   },
   {
     path: 'recover', loadChildren: () => import('app/modules/recover-account/recover-account.module').then(m => m.RecoverAccountModule)
